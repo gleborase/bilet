719cb72bab65b361cc9879ac9cf018c6439694a0
gleborase <krasnopolsci@gmail.com>
2025-05-27 11:19:40 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        ////1 билет
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x=");
+        //    int x = int.Parse(Console.ReadLine());
+        //    Console.Write("y=");
+        //    int y = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"x+y={x + y}");
+        //    Console.Read();
+        //}
+
+
+        ////2 билет
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x=");
+        //    int x = int.Parse(Console.ReadLine());
+        //    Console.Write("y=");
+        //    int y = int.Parse(Console.ReadLine());
+        //    Console.Write("z=");
+        //    int z = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"x+y+z={x+y+z}");
+        //    Console.Read();
+        //}
+
+
+        ////3 билет
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x=");
+        //    double x = double.Parse(Console.ReadLine());
+        //    Console.Write("y=");
+        //    double y = double.Parse(Console.ReadLine());
+        //    Console.WriteLine($"x*y={x * y:f1}");
+        //    Console.Read();
+        //}
+
+
+        ////8 билет
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Номинал купюры = ");
+        //    int x = int.Parse(Console.ReadLine());
+        //    Console.Write("Количество купюр = ");
+        //    int y = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"Сумма денег = {x * y:f2} р.");
+        //    Console.Read();
+        //}
+
+
+        ////13 билет
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x=");
+        //    int x = int.Parse(Console.ReadLine());          
+        //    Console.WriteLine($"десятки = {x / 10}");
+        //    Console.WriteLine($"единицы = {x % 10}");
+        //    Console.Read();
+        //}
+
+        ////билет 14
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x=");
+        //    int x = int.Parse(Console.ReadLine());
+        //    double y = 0;
+        //    if (x < 0)
+        //    {
+        //        y = 6 - x*x;
+        //    }
+        //    else
+        //    {
+        //        y=(Math.Sqrt(x) + 2 * Math.Sin(x));
+        //    }
+        //    Console.WriteLine($"y={y:f2}");
+        //    Console.Read();
+        //}
+
+        ////билет 15
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x=");
+        //    int x = int.Parse(Console.ReadLine());
+        //    double y = 0;
+        //    if (x <= -5)
+        //    {
+        //        y = (Math.Pow(x, 2) + 15 * x);
+        //    }
+        //    else if (x >= 5)
+        //    {
+        //        y = (3 * x + 1) / 5;
+        //    }
+        //    else
+        //    {
+        //        y = (Math.Sqrt(13 + x));
+        //    }
+        //    Console.WriteLine($"y={y:f2}");
+        //    Console.Read();
+        //}
+
+        //    //билет 16
+        //    static void Main(string[] args)
+        //    {
+        //        Console.Write("x=");
+        //        int x = int.Parse(Console.ReadLine());
+        //        double z = 0;
+        //        if (x >= 0)
+        //        {
+        //            z = 2 + 3 * Math.Sqrt(x);
+        //        }
+        //        else
+        //        {
+        //            z = 1 - 2 * Math.Pow(x, 2);
+        //        }               
+        //        Console.WriteLine($"z={z:f2}");
+        //        Console.Read();
+        //    }
+
+        ////билет 18
+        //static void Main(string[] args) 
+        //{
+        //    Console.Write("x=");
+        //    int x= int.Parse(Console.ReadLine());
+        //    double y = 0;
+        //    if (x > 0)
+        //    {
+        //        y = x+1;
+        //    }
+        //    else 
+        //    {
+        //        y = x-2;
+        //    }
+        //    Console.WriteLine($"y={y}");
+        //    Console.Read();
+        //}
+
+        ////билет 19
+        //static void Main(string[] args) 
+        //{
+        //    Console.Write("x=");
+        //    int x = int.Parse(Console.ReadLine());
+        //    int y = 0;
+        //    if (x > 0)
+        //    {
+        //        y = x+1;
+        //    }
+        //    if (x < 0) 
+        //    {
+        //        y= x - 2;
+        //    }
+        //    if (x == 0)
+        //    {
+        //        y = 10;
+        //    }
+        //    Console.WriteLine($"y={y}");
+        //    Console.Read();
+        //}
+    }
+}
